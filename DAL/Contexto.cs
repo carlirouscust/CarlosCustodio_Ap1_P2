22b69847ee5ee4c2b215352e7e0b8c5f6a2911a2
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-17 18:39:44 -0400

    se agrego el modelo al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarlosCustodio_Ap1_P2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarlosCustodio_Ap1_P2.DAL;
 
@@ -7,4 +8,5 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options)
     : base(options) { }
 
+    public DbSet<Registro> Registro { get; set; }
 }
