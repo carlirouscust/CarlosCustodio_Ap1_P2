e6f7de763e6e05cd5c2e7f586744ce96a31304ed
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-17 18:30:53 -0400

    Se creo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarlosCustodio_Ap1_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options)
+    : base(options) { }
+
+}
