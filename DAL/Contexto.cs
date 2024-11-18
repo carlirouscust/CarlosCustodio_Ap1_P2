d65d99c344a34d8791712a80ca30f16a83aa8a29
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-18 18:45:45 -0400

    se creo el model creating y se cambio el nombre del modelo en el dbset

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,46 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options)
     : base(options) { }
 
-    public DbSet<Registro> Registro { get; set; }
+    public DbSet<Combos> combo { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
+        {
+            new Articulos
+            {
+                articuloId = 1,
+                descripcion = "RAM Corsair 16GB",
+                costo = 2500.00m,
+                precio = 5000.00m,
+                existencia = 100
+            },
+            new Articulos
+            {
+                articuloId = 2,
+                descripcion = "Case Corsair",
+                costo = 1500.00m,
+                precio = 3000.00m,
+                existencia = 100
+            },
+            new Articulos
+            {
+                articuloId = 3,
+                descripcion = "Intel i7 11va Gen",
+                costo = 15000.00m,
+                precio = 35000.00m,
+                existencia = 100
+            },
+            new Articulos
+            {
+                articuloId = 4,
+                descripcion = "Power Suply Corsair",
+                costo = 3000.00m,
+                precio = 5000.00m,
+                existencia = 100
+            }
+
+        });
+    }
 }
