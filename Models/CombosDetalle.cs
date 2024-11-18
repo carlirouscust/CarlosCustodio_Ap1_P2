7ebafcd17c8afee76892f4f20fda54d005e86a0a
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-18 18:46:20 -0400

    se creo el modelo combo detalle

---
+++ Models/CombosDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarlosCustodio_Ap1_P2.Models;
+
+public class CombosDetalle
+{
+	[Key]
+	public int detalleId { get; set; }
+
+	[Required(ErrorMessage = "El campo es obligatorio")]
+
+	public Combos? combos { get; set; }
+	[ForeignKey("combos")]
+	public int combosId { get; set; }
+
+	public Articulos? articulos { get; set; }
+	[ForeignKey("articulos")]
+	public int articuloId { get; set; }
+
+	[Required(ErrorMessage = "El campo es obligatorio")]
+	public int? cantidad { get; set; }
+
+	[Required(ErrorMessage = "El campo es obligatorio")]
+	public decimal? costo { get; set; }
+	
+	
+}
